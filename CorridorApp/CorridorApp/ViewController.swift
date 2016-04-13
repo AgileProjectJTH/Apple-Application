c92fab1be0015ae2b8b6398ff7e07ef4eb9bf6fc
Tim Hertzberg <tim_hertzberg@hotmail.com>
2016-04-13 17:14:22 +0200

    Tim: Added some UIControls with some basic functionality

--- CorridorApp/CorridorApp/ViewController.swift
+++ CorridorApp/CorridorApp/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var buttonView: UIView!
+    @IBOutlet weak var mainButton: UIButton!
+    
+    var isAvailable: Bool!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.isAvailable = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClicked(sender: AnyObject) {
+        
+        if self.isAvailable == true {
+            self.isAvailable = false
+            self.mainButton.setTitle("Available", forState: .Normal)
+            self.buttonView.backgroundColor = UIColor.greenColor()
+            self.infoLabel.text = "You are Unavailable, tap to change status to Available"
+        }
+        else {
+            self.isAvailable = true
+            self.mainButton.setTitle("Unvailable", forState: .Normal)
+            self.buttonView.backgroundColor = UIColor.redColor()
+            self.infoLabel.text = "You are Available, tap to change status to Unavailable"
+        }
+    }
 
 }
 
